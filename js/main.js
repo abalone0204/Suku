6320555308a08846d41df882b4e6752e10fae51f
abalone0204 <abalone0204@gmail.com>
2015-09-25 16:51:01 +0800

    add hot module reload

--- js/main.js
+++
@@ -1,2 +0,0 @@
-require('../stylesheets/style.scss');
-console.log('test');
---
+++ src/main.js
@@ -0,0 +1,4 @@
+require('../static/stylesheets/style.scss');
+import React from 'react';
+import Hello from './components/Hello';
+React.render(<Hello />, document.getElementsByClassName('container')[0]);
