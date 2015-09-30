1c27bd68b13add5320eb97ccf0eb0a7b2e59d3f3
abalone0204 <abalone0204@gmail.com>
2015-10-01 02:28:15 +0800

    add orbit

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -5,7 +5,16 @@ export default class Hello {
     return (
         <div className="greeting">
           <h1>Suku</h1>
-          <div id="triangle"></div>
+          <div id="suku-logo">
+            <div id="dac_man">
+              <div id="dac-upper" className="dac-part"></div>
+              <div id="dac-lower" className="dac-part"></div>
+            </div>
+            <div className="orbital"></div>
+            <div className="orbital"></div>
+            <div className="orbital"></div>
+          </div>
+          
           <p>
             sudoDennyKu
           </p>
